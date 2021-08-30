20f852d6bd8b1860bfa210715d71c99b90ec5a21
AndreyVolokhatyy <ftrixagain@gmail.com>
2021-08-30 19:29:10 +0300

    HW_2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
+import ru.levelup.homework_number_two.task_number_one.Calculator;
+
+import java.io.IOException;
+
 public class Main {
-
-    public static void main(String[] args) {
-
+    public static void main (String[] args) throws IOException {
+        Calculator calc = new Calculator();
+        calc.calculate();
     }
 }
