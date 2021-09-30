408f18c581f974916a6438867d5299d22fc96847
AndreyVolokhatyy <ftrixagain@gmail.com>
2021-09-30 17:26:08 +0300

    HW_7

--- src/test/java/ru/levelup/homework_number_six/task_number_one/registration/RegistrationMailTest.java
+++ src/test/java/ru/levelup/homework_number_six/task_number_one/registration/RegistrationMailTest.java
@@ -8,6 +8,7 @@ import ru.levelup.homework_number_six.task_number_one.registration.page.Registra
 
 public class RegistrationMailTest extends RegistrationPage {
       private static final Faker FAKER = new Faker();
+      PropertyData testData = new PropertyData();
 
 
   @Test
@@ -15,12 +16,12 @@ public class RegistrationMailTest extends RegistrationPage {
       String myTheme = FAKER.name().name();
       String myBody = FAKER.book().title();
 
-      login("test_level_up", "Zxcnbv11");
+      login(testData.getData("login"), testData.getData("password"));
       Assert.assertEquals(checkMailPage(),true);
 
-      createSend("ftrix@yandex.ru", myTheme,myBody, false);
+      createSend(testData.getData("email.to.send"), myTheme,myBody, false);
       openDraft();
-      Verify.verify(checkTextAttribute("ftrix@yandex.ru"), "Email не найден");
+      Verify.verify(checkTextAttribute(testData.getData("email.to.send")), "Email не найден");
       Verify.verify(checkTextAttribute(myTheme), "Тема письма не найдена");
       Verify.verify(checkTextAttribute(myBody), "Текст письма не найден");
 
@@ -28,7 +29,7 @@ public class RegistrationMailTest extends RegistrationPage {
       Verify.verify(!emptyTextAttribute(myTheme), "Письмо не отправлено");
 
       openSendsForm();
-      Verify.verify(checkTextAttribute("ftrix@yandex.ru"), "Email не найден");
+      Verify.verify(checkTextAttribute(testData.getData("email.to.send")), "Email не найден");
       Verify.verify(checkTextAttribute(myTheme), "Тема письма не найдена");
       Verify.verify(checkTextAttribute(myBody), "Текст письма не найден");
 
@@ -40,23 +41,23 @@ public class RegistrationMailTest extends RegistrationPage {
       String myTheme = FAKER.name().firstName();
       String myBody = FAKER.book().title();
 
-      login("test_level_up@mail.ru", "Zxcnbv11");
+      login(testData.getData("login"), testData.getData("password"));
       Assert.assertEquals(checkMailPage(),true);
 
       openTestForm();
       int startCount = countLetter();
 
-      createSend("test_level_up@mail.ru", "Тест_"+myTheme ,"Тест_"+myBody, true);
+      createSend(testData.getData("email"), testData.getData("first.word")+myTheme ,"Тест_"+myBody, true);
       openSendsForm();
-      Verify.verify(checkTextAttribute("test_level_up@mail.ru"), "Email не найден");
-      Verify.verify(checkTextAttribute("Тест_"+myTheme), "Тема письма не найдена");
-      Verify.verify(checkTextAttribute("Тест_"+myBody), "Текст письма не найден");
+      Verify.verify(checkTextAttribute(testData.getData("email")), "Email не найден");
+      Verify.verify(checkTextAttribute(testData.getData("first.word")+myTheme), "Тема письма не найдена");
+      Verify.verify(checkTextAttribute(testData.getData("first.word")+myBody), "Текст письма не найден");
 
       openTestForm();
       Verify.verify(countLetter()>startCount, "Письмо не добавлено в папку");
-      Verify.verify(checkEmailAttribute("Тест_"+myTheme,"test_level_up@mail.ru"), "Email не найден");
-      Verify.verify(checkTextAttribute("Тест_"+myTheme), "Тема письма не найдена");
-      Verify.verify(checkTextAttribute("Тест_"+myBody), "Текст письма не найден");
+      Verify.verify(checkEmailAttribute(testData.getData("first.word")+myTheme,"test_level_up@mail.ru"), "Email не найден");
+      Verify.verify(checkTextAttribute(testData.getData("first.word")+myTheme), "Тема письма не найдена");
+      Verify.verify(checkTextAttribute(testData.getData("first.word")+myBody), "Текст письма не найден");
 
       logout();
   }
@@ -66,23 +67,23 @@ public class RegistrationMailTest extends RegistrationPage {
     String myTheme = FAKER.name().firstName();
     String myBody = FAKER.book().title();
 
-    login("test_level_up@mail.ru", "Zxcnbv11");
+    login(testData.getData("login"), testData.getData("password"));
     Assert.assertEquals(checkMailPage(),true);
 
     int startCount = countLetter();
     openDeleteForm();
     int deleteCount = countLetter();
-    createSend("test_level_up@mail.ru", myTheme, myBody, true);
+    createSend(testData.getData("email"), myTheme, myBody, true);
 
     openInnerLetter();
     Verify.verify(countLetter()>startCount, "Письмо не добавлено в папку");
-    Verify.verify(checkEmailAttribute(myTheme,"test_level_up@mail.ru"), "Email не найден");
+    Verify.verify(checkEmailAttribute(myTheme,testData.getData("email")), "Email не найден");
     Verify.verify(checkTextAttribute(myTheme), "Тема письма не найдена");
     Verify.verify(checkTextAttribute(myBody), "Текст письма не найден");
 
     openAndDeleteLetter(myTheme);
     Verify.verify(countLetter()>deleteCount, "Письмо не добавлено в папку");
-    Verify.verify(checkEmailAttribute(myTheme,"test_level_up@mail.ru"), "Email не найден");
+    Verify.verify(checkEmailAttribute(myTheme,testData.getData("email")), "Email не найден");
     Verify.verify(checkTextAttribute(myTheme), "Тема письма не найдена");
     Verify.verify(checkTextAttribute(myBody), "Текст письма не найден");
 
